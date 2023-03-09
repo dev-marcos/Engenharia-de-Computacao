d3848bdba647528e8a7bdcef39be8f169e31fe6b
Marcos Rocha <37712508+dev-marcos@users.noreply.github.com>
2023-03-08 21:10:47 -0300

    Add files via upload

---
+++ Fundamentos de Programaçao/Lista 4/Exe_07.c
@@ -0,0 +1,40 @@
+/*
+7) Modifique o programa anterior para imprimir os números em ordem crescente.
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    int n1, n2, n3;
+
+    printf("Forneca 3 numeros inteiros diferentes: ");
+    scanf("%d %d %d", &n1, &n2, &n3);
+
+    if (n1 < n2 && n2 < n3)
+    {
+        printf("A ordem crescente e: %d %d %d", n1, n2, n3);
+    }
+    if (n1 < n3 && n3 < n2)
+    {
+        printf("A ordem crescente e: %d %d %d", n1, n3, n2);
+    }
+    if (n2 < n1 && n1 < n3)
+    {
+        printf("A ordem crescente e: %d %d %d", n2, n1, n3);
+    }
+    if (n2 < n3 && n3 < n1)
+    {
+        printf("A ordem crescente e: %d %d %d", n2, n3, n1);
+    }
+    if (n3 < n1 && n1 < n2)
+    {
+        printf("A ordem crescente e: %d %d %d", n3, n1, n2);
+    }
+    if (n3 < n2 && n2 < n1)
+    {
+        printf("A ordem crescente e: %d %d %d", n3, n2, n1);
+    }
+
+    return 0;
+}
---
+++ Fundamentos de Programaçao/Lista 4/Exe_09.c
@@ -0,0 +1,12 @@
+/*
+9) Independente de considerações de precedência, porque as declarações “a = a++” e
+“i++ + ++i” são de comportamento indefinido?
+*/
+
+#include <stdio.h>
+
+int main (){
+
+
+    return 0;
+}
